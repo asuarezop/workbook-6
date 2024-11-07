f11f967542784dfb17d29f4872f4a33fe8858993
Anthony Suarez <asuarezop@gmail.com>
2024-11-07 14:26:02 -0500

    Added getMostValuable and getLeastValuable() into Portfolio class

--- PortfolioExercise/src/main/java/com/pluralsight/finance/models/Portfolio.java
+++ PortfolioExercise/src/main/java/com/pluralsight/finance/models/Portfolio.java
@@ -1,23 +1,24 @@
 package com.pluralsight.finance.models;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class Portfolio implements Valuable {
     private String name;
     private String owner;
-    private ArrayList<Asset> assets;
+    private List<Valuable> assets;
 
     public Portfolio(String name, String owner) {
         this.name = name;
         this.owner = owner;
     }
 
-    public void add(Asset a) {
-
-    }
+    public void add(Valuable a) {}
 
     @Override
     public double getValue() {
         return 0;
     }
+
+    public void getMostValuable() {}
+    public void getLeastValuable() {}
 }
